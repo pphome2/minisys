4e3a1ce2c7186661dfa2486326df0180a699157c
Péter <pphome2@gmail.com>
2021-02-15 19:39:38 +0100

    Új verzió.

--- nvr/nvr.php
+++ nvr/nvr.php
@@ -76,7 +76,7 @@ function filetable($dir){
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title><?php echo($SI_NAME); ?></title>
+		<title><?php echo($NVR_NAME); ?></title>
 		<meta charset="utf-8" />
 		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
--- nvr/nvr_video.php
+++ nvr/nvr_video.php
@@ -42,7 +42,7 @@ if (empty($videofile)){
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title><?php echo($SI_NAME); ?></title>
+		<title><?php echo($NVR_NAME); ?></title>
 		<meta charset="utf-8" />
 		<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
