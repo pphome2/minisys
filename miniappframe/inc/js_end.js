f9c87187ba4c9339c5e11f4faaf71da5d9dc4e66
Péter <pphome2@gmail.com>
2021-02-18 15:25:18 +0100

    Új verzió.

--- miniappframe/inc/js_end.js
+++ miniappframe/inc/js_end.js
@@ -1,7 +1,7 @@
 <script>
 
 <?php
-if ($LOGGED_IN){
+if ($MA_LOGGEDIN){
 	if ($MA_ENABLE_COOKIES){
 ?>
 	setTimeout(function () { window.location.href = "<?php echo($MA_ADMINFILE); ?>"; }, <?php echo((($MA_LOGIN_TIMEOUT+1)*1000)); ?>);
